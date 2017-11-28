d12a560aa45c60abfc26b19acfad34f81213da74
Manabu Matsuzaki <matsumana@gmail.com>
2017-11-29 01:43:54 +0900

    add controller

---
+++ src/main/java/info/matsumana/tsujun/controller/RootController.java
@@ -0,0 +1,15 @@
+package info.matsumana.tsujun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    String index() {
+        return "index";
+    }
+}
