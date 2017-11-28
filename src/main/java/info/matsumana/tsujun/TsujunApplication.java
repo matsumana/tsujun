a66925738c1875abe5224dec36b82fbf5a6a576d
Manabu Matsuzaki <matsumana@gmail.com>
2017-11-29 01:11:57 +0900

    add skeleton

---
+++ src/main/java/info/matsumana/tsujun/TsujunApplication.java
@@ -0,0 +1,12 @@
+package info.matsumana.tsujun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsujunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsujunApplication.class, args);
+    }
+}
---
+++ src/test/java/info/matsumana/tsujun/TsujunApplicationTest.java
@@ -0,0 +1,16 @@
+package info.matsumana.tsujun;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TsujunApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
