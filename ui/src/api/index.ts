143fffc627619cea0fce34dc3df711a8f60043e1
Manabu Matsuzaki <matsumana@gmail.com>
2017-12-28 23:24:46 +0900

    fix

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -2,7 +2,7 @@ import { Request as Req } from '../store/model/Request';
 
 export class Api {
 
-  submit(sequence: number, sql: string, callback: (data: string) => void): void {
+  submit(sequence: number, sql: string, callback: (data: string) => void) {
     const requestBody = new Req();
     requestBody.sequence = sequence;
     requestBody.sql = sql;
@@ -13,15 +13,30 @@ export class Api {
       'X-Requested-With': 'XMLHttpRequest',
     });
 
+    // referred to the follows.
+    // https://www.chromestatus.com/feature/5804334163951616
+    // https://googlechrome.github.io/samples/fetch-api/fetch-response-stream.html
     fetch(`${window.location.protocol}//${window.location.host}/sql`, {
       method: 'POST',
       headers,
       body: JSON.stringify(requestBody),
     }).then((response) => {
-      response.body.getReader().read().then((result) => {
-        const rows = String.fromCharCode.apply('', new Uint16Array(result.value));
-        callback(rows);
-      });
+      return this.pump(response.body.getReader(), callback);
     });
+  }
+
+  pump(reader: ReadableStreamReader, callback: (data: string) => void) {
+    reader.read().then(
+        (result) => {
+          if (result.done) {
+            return;
+          }
+
+          const rows = String.fromCharCode.apply('', new Uint16Array(result.value));
+          callback(rows);
+
+          return this.pump(reader, callback);
+        }
+    );
   }
 }
